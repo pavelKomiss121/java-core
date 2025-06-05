80e0815903ddd2700227b12b4d4be458b1f520aa
Komissarov Pavel <pavelkomiss@gmail.com>
2025-06-06 00:23:06 +0300

    mp-18 добавление строки с именем и возрастом

---
+++ src/main/java/ru/mentee/power/Main.java
@@ -0,0 +1,11 @@
+package ru.mentee.power;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 21;
+        String myName = "Паша";
+        System.out.println("Меня зовут " + myName + " мне " + myAge + " лет.");
+    }
+}
