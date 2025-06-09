5ce7220b3503ef5e526debb454ff328c901f01e0
Komissarov Pavel <pavelkomiss@gmail.com>
2025-06-09 14:01:44 +0300

    mp-25 Доработка калькулятора

--- src/main/java/ru/mentee/power/conditions/CreditCalculator.java
+++ src/main/java/ru/mentee/power/conditions/CreditCalculator.java
@@ -9,13 +9,13 @@ public class CreditCalculator {
     // TODO: Объявите константы для базовых процентных ставок по типам кредитов
     // TODO: Объявите константы для диапазонов кредитного рейтинга
     // TODO: Объявите константы для диапазонов суммы кредита и срока кредита
-    private final float mortgage = 0.09f;
-    private final float consumer = 0.15f;
-    private final float credit = 0.12f;
+    private static final float MORTGAGE = 0.09f;
+    private static final float CONSUMER = 0.15f;
+    private static final float CREDIT = 0.12f;
 
-    private final float excellent = -0.02f;
-    private final float good = -0.01f;
-    private final float bad = 0.03f;
+    private static final float EXCELLENT = -0.02f;
+    private static final float GOOD = -0.01f;
+    private static final float BAD = 0.03f;
     /**
      * Рассчитывает ежемесячный платеж по кредиту
      *
@@ -27,8 +27,8 @@ public class CreditCalculator {
      */
     public double calculateMonthlyPayment(double loanAmount, int loanTermMonths, String creditType, int creditScore) {
         // TODO: Реализуйте метод согласно требованиям
-        double credit = 0;
-        double creditRating = 0;
+        double creditRate = 0;
+        double creditModifier = 0;
         if (!creditType.equals("Ипотека") && !creditType.equals("Потребительский") && !creditType.equals("Автокредит")) {
             return -1;
         }
@@ -37,25 +37,25 @@ public class CreditCalculator {
         }
         switch (creditType) {
             case ("Ипотека"):
-                credit = mortgage;
+                creditRate  = MORTGAGE;
                 break;
             case ("Потребительский"):
-                credit = consumer;
+                creditRate  = CONSUMER;
                 break;
             case ("Автокредит"):
-                credit = this.credit;
+                creditRate  = this.CREDIT;
                 break;
         }
         if (creditScore >850){
             return -1;
         } else if (creditScore <= 850 && creditScore >= 750) {
-            creditRating = excellent;
+            creditModifier = EXCELLENT;
         } else if (creditScore >= 650) {
-            creditRating = good;
+            creditModifier = GOOD;
         } else if (creditScore >= 500) {
-            creditRating = 0;
+            creditModifier = 0;
         } else if (creditScore >= 300) {
-            creditRating = bad;
+            creditModifier = BAD;
         } else {
             return -1;
         }
@@ -63,7 +63,7 @@ public class CreditCalculator {
         if (loanTermMonths < 1 || loanTermMonths > 360){
             return -1;
         }
-        double monthlyRate = (creditRating + credit)/12;
+        double monthlyRate = (creditModifier + creditRate)/12;
         double monthlyPayment = loanAmount * (monthlyRate * Math.pow(1 + monthlyRate, loanTermMonths)) / (Math.pow(1 + monthlyRate, loanTermMonths) - 1);
         return  monthlyPayment;
 
